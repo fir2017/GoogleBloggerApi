ac09ca9000ee709351aaca18c68ea63a51f4b64c
kent42ilink <kent42.ilink@gmail.com>
2016-07-31 11:42:18 +0800

    加入專案檔案。

---
+++ GoogleBloggerApi/Common/BloggerHelp.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleBloggerApi.Common
+{
+    public class BloggerHelp
+    {
+        /// <summary>
+        /// 憑證檔案路徑
+        /// </summary>
+        private string KeyFilePath = "C:\\auth2.json";
+
+        /// <summary>
+        /// 取得使用者資訊
+        /// </summary>
+        /// <returns></returns>
+        public UserCredential GetCredential()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream(KeyFilePath, FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { "https://www.googleapis.com/auth/blogger" },
+                    "Kentilink",
+                    CancellationToken.None,
+                    new FileDataStore("Auth.Blogger")).Result;
+            }
+
+            return credential;
+        }
+    }
+}
---
+++ GoogleBloggerApi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Google.Apis.Blogger.v3.Data;
+using Google.Apis.Blogger.v3;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+using System.Threading;
+using Google.Apis.Util.Store;
+using System.Collections.Generic;
+using GoogleBloggerApi.Common;
+
+
+namespace GoogleBloggerApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BloggerHelp BloggerHelp = new BloggerHelp();
+
+            string blogUrl = "blogUrl.com";
+
+            BloggerService BloggerService = new BloggerService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = BloggerHelp.GetCredential(),
+                ApplicationName = "Test",
+            });
+
+            //取得Blogger資料
+            var BlogResource = BloggerService.Blogs.GetByUrl(blogUrl);
+
+            //第一次會請求允許
+            Blog blog = BlogResource.Execute();
+            
+            Console.WriteLine("Blog ID: " + blog.Id);
+            Console.WriteLine("Insert");
+
+            //新增資料
+            Post PostData = new Post();
+            PostData.Content = "<h1>Content</h1>";
+            PostData.Labels = new List<string>() { "TestTag" };
+            PostData.Title = DateTime.Now.ToString() + "Title";
+
+            //執行新增
+            PostsResource PostsResource = new PostsResource(BloggerService);
+            PostsResource.Insert(PostData, blog.Id).Execute();
+
+            Console.WriteLine("Done...");
+
+
+        }
+    }
+}
